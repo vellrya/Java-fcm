aed1dc6960bc4c1f29fda351716acfb7b2ecda06
Rogier Saarloos <rsaarloos@virtual-affairs.com>
2021-03-01 16:48:53 +0100

    added FCMOptions

--- src/main/java/org/pixsee/fcm/FCMOptions.java
+++ src/main/java/org/pixsee/fcm/FCMOptions.java
@@ -2,4 +2,8 @@ package org.pixsee.fcm;
 
 public class FCMOptions {
     private String analytics_label;
+
+    public FCMOptions(String analyticsLabel) {
+        this.analytics_label = analyticsLabel;
+    }
 }
